eb8ec238e8246805fb344f9b9c93bcfdf524ce2c
Padrigon, Lynbert <padrigonlynbert@gmail.com>
2025-05-09 17:33:16 +0800

    add: allowall xframe

--- test_integ_dj/settings.py
+++ test_integ_dj/settings.py
@@ -22,11 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
-
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+X_FRAME_OPTIONS = 'ALLOWALL'
 
 # Application definition
 
