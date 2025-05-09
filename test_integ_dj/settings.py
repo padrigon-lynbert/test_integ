8ec85ad99100687afb7c4eec7a4a7b84e671819a
Padrigon, Lynbert <padrigonlynbert@gmail.com>
2025-05-09 16:45:56 +0800

    fix: prod config

--- test_integ_dj/settings.py
+++ test_integ_dj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS", "127.0.0.1")]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 # Application definition
 
