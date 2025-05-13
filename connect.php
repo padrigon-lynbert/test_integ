9c9702324e173b64384f223fabd3539a8af07e73
Padrigon, Lynbert <padrigonlynbert@gmail.com>
2025-05-13 16:35:14 +0800

    add: iframe domain url

--- connect.php
+++ connect.php
@@ -42,27 +42,79 @@ while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
 }
 ?>
 
-<h1>user_info, cant unhash this unfortunately</h1>
+<!-- <h1>user_info, cant unhash this unfortunately</h1> -->
 <?php
 
 // select * from user_info 
-$query = $pdo->query("SELECT * FROM user_info");
+// $query = $pdo->query("SELECT * FROM user_info");
 
 // Loop, display each row
-while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
-    echo "ID: " . $row['id'] . "<br>";
-    echo "Name: " . $row['name'] . "<br>";
-    echo "Email: " . $row['email'] . "<br>";
-    echo "Password: " . $row['password'] . "<br>";
-    echo "Info: " . $row['info'] . "<br>";
-    echo "Bid: " . $row['bid'] . "<br>";
-    echo "Image: " . $row['img'] . "<br>";
-    echo "<hr>";  // line to separate output
-}
+// while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+//     echo "ID: " . $row['id'] . "<br>";
+//     echo "Name: " . $row['name'] . "<br>";
+//     echo "Email: " . $row['email'] . "<br>";
+//     echo "Password: " . $row['password'] . "<br>";
+//     echo "Info: " . $row['info'] . "<br>";
+//     echo "Bid: " . $row['bid'] . "<br>";
+//     echo "<hr>";  // line to separate output
+// }
 ?>
 
 
-<h1>frame domain</h1>
-<div style="padding: 10%;">
-  <iframe src="https://www.wikipedia.org" width="100%" height="500px"></iframe>
+<h1>frame wikipedia(example)</h1>
+<div style="padding: 5%;">
+  <!-- <iframe src="https://www.wikipedia.org" width="100%" height="500px"></iframe> -->
+  <iframe src="https://www.wikipedia.org" width="100%" height="400px"></iframe>
 </div>
+<h1>Public domain</h1>
+<div style="padding: 5%;">
+  <!-- <iframe src="https://www.wikipedia.org" width="100%" height="500px"></iframe> -->
+  <iframe src="https://logistics-2-kft6.onrender.com" width="100%" height="300px"></iframe>
+</div>
+<h1>user domain</h1>
+<div style="padding: 5%;">
+  <!-- <iframe src="https://www.wikipedia.org" width="100%" height="500px"></iframe> -->
+  <iframe src="https://logistics-1-0er9.onrender.com" width="100%" height="300px"></iframe>
+</div>
+<h1>admin domain</h1>
+<div style="padding: 5%;">
+  <!-- <iframe src="https://www.wikipedia.org" width="100%" height="500px"></iframe> -->
+  <iframe src="https://admin-domain.onrender.com" width="100%" height="300px"></iframe>
+</div>
+
+<?php
+$host = "dpg-d0e74ch5pdvs73aqmnr0-a.oregon-postgres.render.com";
+$port = "5432";
+$dbname = "logistic2";
+$user = "logistic2_user";
+$password = "RnOMkUBoAX06KJ7crw3mBgl68pN3ISyf";
+
+    try {
+        $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;";
+        $pdo = new PDO($dsn, $user, $password);
+
+        // Get the first user ordered by primary key (assume id)
+        $sql = "SELECT * FROM user_info ORDER BY id ASC LIMIT 1";
+        $stmt = $pdo->query($sql);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($row) {
+            echo "<h2>User Details</h2>";
+            echo "ID: " . $row['id'] . "<br>";
+            echo "Name: " . $row['name'] . "<br>";
+            echo "Email: " . $row['email'] . "<br>";
+            echo "Password: " . $row['password'] . "<br>";
+            echo "Info: " . $row['info'] . "<br>";
+            echo "Bid: " . $row['bid'] . "<br>";
+
+            // If 'img' is a path or URL
+            // echo "<img src='" . $row['img'] . "' width='300' alt='User Image'><br>";
+        } else {
+            echo "No user found.";
+        }
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+    ?>
+
+    <h1>Image > migrate again to store blob instead</h1>
